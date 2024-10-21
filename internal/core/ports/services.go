6fc02f8f624e334317567bd1e6863fdf55322ee9
JoseFredes <jose.fredes@mayor.cl>
2024-10-20 21:34:10 -0300

    fix: typo error

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -2,6 +2,6 @@ package ports
 
 import "github.com/JoseFredes/learn-go-hex/internal/core/domain"
 
-type UsersServices interface {
+type UserServices interface {
 	Get(id int) (domain.User, error)
 }
