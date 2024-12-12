7974271ff64abbd5cc31f09254afca8ce2a505a0
JoseFredes <jose.fredes@mayor.cl>
2024-12-12 15:32:00 -0300

    refactor: Refactor main.go to use database/sql for MySQL connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,29 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	sql "database/sql"
 
 	"github.com/JoseFredes/learn-go-hex/internal/infra/left"
 	"github.com/JoseFredes/learn-go-hex/internal/infra/right"
 	services "github.com/JoseFredes/learn-go-hex/internal/services/user"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	DNS = "user:password@tcp(127.0.0.1:3306)/db"
 )
 
 func main() {
-	dsn := "user:password@tcp(127.0.0.1:3306)/mydatabase" 
-	
-	userRepository, err := right.NewMySQLUserRepository(dsn)
+	db, err := sql.Open("mysql", DNS)
 	if err != nil {
-		log.Fatalf("Error al conectar con la base de datos: %v", err)
+		return
 	}
 
+	userRepository, err := right.NewMySQLUserRepository(db)
+	if err != nil {
+		return
+	}
 
 	userService := services.NewUserServices(userRepository)
 
-
 	userHandler := left.NewUserHandler(userService)
 
-	http.HandleFunc("/users/", userHandler.GetUser)
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			userHandler.GetUser(w, r)
+		case http.MethodPost:
+			userHandler.CreateUser(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
