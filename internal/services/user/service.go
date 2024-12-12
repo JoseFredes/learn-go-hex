b65584191979c10257ef71d619e0aa6de22a3551
JoseFredes <jose.fredes@mayor.cl>
2024-12-12 15:32:31 -0300

    Refactor UserServices to implement Create method and add validation for new users

--- internal/services/user/service.go
+++ internal/services/user/service.go
@@ -5,14 +5,27 @@ import (
 	"github.com/JoseFredes/learn-go-hex/internal/core/ports"
 )
 
+type UserServices interface {
+	Get(id int) (*domain.User, error)
+	Create(user *domain.User) (*domain.User, error)
+}
+
 type userServices struct {
 	repository ports.UserRepository
 }
 
-func NewUserServices(repo ports.UserRepository) *userServices {
-	return &userServices{repository: repo}
+func NewUserServices(repository ports.UserRepository) UserServices {
+	return &userServices{repository: repository}
 }
 
-func (s *userServices) Get(id int) (domain.User , error){
+func (s *userServices) Get(id int) (*domain.User, error) {
 	return s.repository.GetUserByID(id)
+}
+
+func (s *userServices) Create(user *domain.User) (*domain.User, error) {
+	if err := user.ValitateNewUser(); err != nil {
+		return nil, err
+	}
+
+	return s.repository.CreateUser(user)
 }
