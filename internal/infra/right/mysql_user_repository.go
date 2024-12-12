10a04289a23d2fea087d00958c13f52a37ad66fb
JoseFredes <jose.fredes@mayor.cl>
2024-12-12 15:32:26 -0300

    Refactor MySQLUserRepository to implement CreateUser method

--- internal/infra/right/mysql_user_repository.go
+++ internal/infra/right/mysql_user_repository.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/JoseFredes/learn-go-hex/internal/core/domain"
+	"github.com/JoseFredes/learn-go-hex/internal/core/ports"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -12,16 +13,12 @@ type MySQLUserRepository struct {
 	db *sql.DB
 }
 
-func NewMySQLUserRepository(dsn string) (*MySQLUserRepository, error) {
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
+func NewMySQLUserRepository(db *sql.DB) (ports.UserRepository, error) {
 	return &MySQLUserRepository{db: db}, nil
 }
 
-func (r *MySQLUserRepository) GetUserByID(id int) (domain.User, error) {
-	query := "SELECT id, name, lastname, email FROM users WHERE id = ?"
+func (r *MySQLUserRepository) GetUserByID(id int) (*domain.User, error) {
+	query := "SELECT id, name, last_name, email FROM users WHERE id = ?"
 	row := r.db.QueryRow(query, id)
 
 	var user domain.User
@@ -29,10 +26,38 @@ func (r *MySQLUserRepository) GetUserByID(id int) (domain.User, error) {
 	err := row.Scan(&user.ID, &user.Name, &user.LastName, &user.Email)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return domain.User{}, fmt.Errorf("user not found")
+			return nil, fmt.Errorf("user not found")
 		}
-		return domain.User{}, err
+		return nil, err
 	}
+
+	return &user, nil
+}
+
+func (r *MySQLUserRepository) CreateUser(user *domain.User) (*domain.User, error) {
+	query := "INSERT INTO users (name, last_name, email) VALUES (?, ?, ?)"
+
+	result, err := r.db.Exec(query, user.Name, user.LastName, user.Email)
+	println(query)
+	if err != nil {
+		return nil, err
+	}
+
+	print(result)
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	if rows, err := result.RowsAffected(); err != nil {
+		return nil, err
+	} else {
+		fmt.Println(rows)
+	}
+
+	fmt.Println(result)
+	user.ID = int(id)
 
 	return user, nil
 }
