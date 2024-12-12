6be990f553b059c2f7342b1424e2dca5f4d3775f
JoseFredes <jose.fredes@mayor.cl>
2024-12-12 15:32:16 -0300

    Refactor UserHandler to handle user requests and add CreateUser method

--- internal/infra/left/user_handler.go
+++ internal/infra/left/user_handler.go
@@ -2,39 +2,63 @@ package left
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"strconv"
 
-	"net/http"
-
-	"github.com/JoseFredes/learn-go-hex/internal/core/ports"
+	"github.com/JoseFredes/learn-go-hex/internal/infra/left/dto"
+	service "github.com/JoseFredes/learn-go-hex/internal/services/user"
 )
 
 type UserHandler struct {
-	service ports.UserServices
+	service service.UserServices
 }
 
-func NewUserHandler(service ports.UserServices) *UserHandler {
+func NewUserHandler(service service.UserServices) *UserHandler {
 	return &UserHandler{service: service}
 }
 
 func (h *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
 	idParam := r.URL.Query().Get("id")
-    if idParam == "" {
-        http.Error(w, "Missing 'id' parameter", http.StatusBadRequest)
-        return
-    }
+	if idParam == "" {
+		http.Error(w, "Missing 'id' parameter", http.StatusBadRequest)
+		return
+	}
 
 	id, err := strconv.Atoi(idParam)
-	if err != nil{
+	if err != nil {
 		http.Error(w, "Id is not a number", http.StatusBadRequest)
 	}
 
 	users, err := h.service.Get(id)
 	if err != nil {
-		http.Error(w, "Failed to get user", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Failed to get user: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(users)
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.UserDTO
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Failed to parse user", http.StatusBadRequest)
+		return
+	}
+
+	if err := user.ValitateNewUser(); err != nil {
+		http.Error(w, fmt.Sprintf("Invalid user: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	userCreated, err := h.service.Create(user.ParseToDomain())
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to create user: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(userCreated)
 }
